a98a4c9906d9e92cd4e514fdf779cf30fdb584a0
cristian tilano silva <cristiantilanosilva@gmail.com>
2019-11-19 20:07:29 -0500

    primer taller de php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>TIENDA DE ROPA EL MOCHO</title>
+</head>
+<body>
+<font face="Algerian"</font>
+<H1><center><p style="color:red";>TIENDA DE ROPA EL MOCHO
+<body background="file://user_quota/data05$/SE-SCHOOL3946/Desktop/Pagina%20mela/Descargar%20pack%20fondos%20abstractos%20%20(455).jpg";>
+<header>
+<h6>Tienda de ropa en lina y con puntos de venta en varios sitios</h6>
+<nav>
+
+</header>
+<img src="prenda1.jpg"widht="300" height="300"/>
+<img src="tienda.jpg"widht="300" height="300"/>
+
+<ul id=”button”>
+ <li><a href="Pagina2">Inicio</a></li>
+ <li><a href="Pagina3 ">Sobre nosotros</a></li>
+ <li><a href="Pagina4">Productos</a></li>
+ <li><a href="Pagina5">Contacto</a></li>
+ </ul>
+</nav>
+
+
+
+</body>
+</html>
